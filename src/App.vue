b3f709a248097bb91381b16dbe8e984e966d88c4
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-04-19 17:36:57 +0100

    Inizio

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderDiTesta from './components/HeaderDiTesta.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderDiTesta
   }
 }
 </script>
